c3a7d9747b0a26e49a642d95dec40244bf9d5648
zawhtetnaing10 <zawgyi.gog@gmail.com>
2025-11-05 10:14:47 +0630

    BM25 search fully complete

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -17,6 +17,17 @@ def handle_build(inv_index: InvertedIndex):
     inv_index.build()
 
 
+def handle_search(inv_index: InvertedIndex, query: str, limit: int):
+    """
+        Handles search command. Searches the index for the given query and print out the results
+    """
+    results = inv_index.bm25_search(query, limit)
+    for idx, result in enumerate(results):
+        print("")
+        print(f"{idx + 1}. {result.metadata['title']}")
+        print(f"{result.page_content}")
+
+
 def main():
     parser = argparse.ArgumentParser(description="Keyword Search CLI")
     subparsers = parser.add_subparsers(
@@ -30,6 +41,13 @@ def main():
     subparsers.add_parser(
         "build", help="Build the inverted index for personal information")
 
+    # Search
+    bm25_search_parser = subparsers.add_parser(
+        "search", help="Do a bm25 search ")
+    bm25_search_parser.add_argument("query", type=str, help="Search query")
+    bm25_search_parser.add_argument(
+        "--limit", type=int, help="Number of results.")
+
     # Keyword search object
     inv_index = InvertedIndex()
 
@@ -40,6 +58,8 @@ def main():
             handle_download_nltk()
         case "build":
             handle_build(inv_index)
+        case "search":
+            handle_search(inv_index, args.query, args.limit)
 
 
 if __name__ == "__main__":
--- lib/bm25_search/inverted_index.py
+++ lib/bm25_search/inverted_index.py
@@ -1,3 +1,4 @@
+import os
 from lib.utils.text_utils import tokenize_text
 from langchain_core.documents import Document
 from langchain_community.document_loaders import JSONLoader
@@ -17,7 +18,11 @@ def metadata_func(record: dict, metadata: dict) -> dict:
 
 
 class InvertedIndex:
+
     def build(self):
+        """
+            Build and save the index for BM25 search
+        """
         try:
 
             # Load json and create documents
@@ -29,14 +34,17 @@ class InvertedIndex:
             )
             documents = loader.load()
 
+            # Transform the documents. Add title before details in page content
+            documents = self._transform_docs(documents)
+
             print(
                 f"Successfully loaded json and created langchain docs")
 
             # Create the index
-            index = BM25Retriever.from_documents(
+            index: BM25Retriever = BM25Retriever.from_documents(
                 documents=documents,
                 preprocess_func=tokenize_text,
-                k=5
+                k=25
             )
 
             print(f"Indexing complete {index}")
@@ -51,3 +59,53 @@ class InvertedIndex:
 
         except FileNotFoundError:
             print(f"Cannot open file in f{constants.ABOUT_ME_FILE_PATH}")
+
+    def _transform_docs(self, docs: list[Document]) -> list[Document]:
+        """
+            Transform the docs by adding the title before the details for page content.
+            This is done for more accurate bm25 search results
+        """
+        result = []
+
+        for doc in docs:
+            title = doc.metadata.get("title", "")
+            # Append title to details
+            new_page_content = f"{title} {doc.page_content}"
+            # Set new page content for the doc
+            doc.page_content = new_page_content
+
+            result.append(doc)
+
+        return result
+
+    def bm25_search(self, query: str, limit: int):
+        """
+            Do a bm25 search from saved index.
+            Return all the results
+        """
+        retriever = self._build_or_load_index()
+
+        # Set the top k results to 1.
+        retriever.k = limit
+
+        # Change the k value after testing
+        docs = retriever.invoke(query)
+
+        return docs
+
+    def _build_or_load_index(self) -> BM25Retriever:
+        """
+            If the index hasn't been built yet, build it first.
+            Then load the index and return it.
+        """
+        # If index does not exists build it first.
+        if not os.path.exists(constants.INDEX_FILE_PATH):
+            self.build()
+
+        # Load the index and return
+        try:
+            with open(constants.INDEX_FILE_PATH, 'rb') as f:
+                index = pickle.load(f)
+                return index
+        except Exception as e:
+            print(f"Error loading the index {e}")
