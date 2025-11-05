a3a0d36ab881c98d07e7e489ba1516d3df8372ce
zawhtetnaing10 <zawgyi.gog@gmail.com>
2025-11-05 08:23:34 +0630

    Added punctuation removal, stop words removal and stemming steps in tokenization

--- lib/utils/text_utils.py
+++ lib/utils/text_utils.py
@@ -1,9 +1,36 @@
+import string
 import nltk
 from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
 
 
-def tokenize_text(text):
+STOP_WORDS = set(stopwords.words('english'))
+STEMMER = PorterStemmer()
+
+
+def tokenize_text(text: str):
     """
         Create tokens for the given text.
     """
-    return word_tokenize(text.lower())
+
+    # Change to lower case
+    text = text.lower()
+
+    # Remove punctuation
+    trans_table = str.maketrans("", "", string.punctuation)
+    text = text.translate(trans_table)
+
+    # Tokenize
+    tokens = word_tokenize(text)
+
+    # Remove stop words
+    filtered_tokens = []
+    for token in tokens:
+        if token not in STOP_WORDS:
+            filtered_tokens.append(token)
+
+    # Stem
+    result = [STEMMER.stem(token) for token in filtered_tokens]
+
+    return result
