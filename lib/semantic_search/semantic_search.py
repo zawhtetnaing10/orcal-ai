ebff6d15fffc775289e60cd18678e33cb4e653e1
zawhtetnaing10 <zawgyi.gog@gmail.com>
2025-11-09 18:57:26 +0630

    Added function to load all documents and chunk every doc

--- lib/semantic_search/semantic_search.py
+++ lib/semantic_search/semantic_search.py
@@ -1,15 +1,33 @@
 from langchain_core.documents import Document
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
+import lib.utils.data_loader_utils as data_loader_utils
+
 
 class SemanticSearch:
+
+    def build_embeddings(self):
+        try:
+            # Load the documents
+            documents = data_loader_utils.load_about_me()
+
+            # Chunk all the docs
+            all_chunks = []
+            for index, doc in enumerate(documents):
+                chunks = self.semantic_chunk(doc, index)
+                all_chunks.extend(chunks)
+
+            # TODO: - Embed and Save
+
+        except Exception as e:
+            print(f"Unable to load the documents {e}")
+
     def semantic_chunk(self, document: Document, doc_idx: int, chunk_size: int = 500, overlap: int = 50) -> list[Document]:
         """
             Chunk the page_content of the input document into chunks. 
             Chunking is done in sentence by sentence format.
         """
 
-        # TODO: - Find out how chunking works.
         # Separator list for sentence awareness
         custom_separators = [
             "\n\n",
