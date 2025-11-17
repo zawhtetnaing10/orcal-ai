acec0fd4a5f1f5c9104ce01c80d15f057c845d21
zawhtetnaing10 <zawgyi.gog@gmail.com>
2025-11-17 09:31:46 +0630

    Added code to exit or quit the session with LLM. Aded psuedo code for Turn History

--- cli/rag_cli.py
+++ cli/rag_cli.py
@@ -11,8 +11,20 @@ def handle_rag(rag: RAG):
         sys.stdout.flush()
         query = sys.stdin.readline().strip()
 
+        # If the query is quit or exit, just exit. Do nothing
+        if query.lower() == "exit" or query.lower() == "quit":
+            print("Quitting the session.")
+            exit(0)
+
+        # Save the current query in Turn History
+
+        # Depending on Turn History, create a new prompt
+
         # Response
         response = rag.discuss(query)
+
+        # Add the response in Turn History
+
         print(f"Assistant: {response}")
 
 
